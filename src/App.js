aaf239f34d4fb9ceb8ff31e9936464693c054a8c
William Bilsdon <william.bilsdon@zynstra.com>
2019-12-09 13:48:33 +0000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class TaskList extends React.Component{
+  constructor(props){
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(task){
+    this.props.deleteItem(task.key);
+  }
+
+  List(task){
+    return <li key={task.key}>{task.value}</li>
+  }
+
+  
+
+  render(){
+    var enteries = this.props.tasks;
+    var listItems = enteries.map(this.List);
+    return(
+        <ul className="ListOfTasks">
+          {listItems}
+        </ul>
+    );
+  }
 }
 
-export default App;
+class AddTodo extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.file = React.createRef();
+  }
+
+  onSubmit(e){
+    e.preventDefault();
+
+    var input = this.file.current.value;
+
+    this.props.addItem(input)
+  }
+
+  render(){
+    return(
+      <div className="todoAdd">
+        <form onSubmit={this.onSubmit}>
+          <input type="text" ref={this.file}/>
+          <button type="submit">+</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+class ToDoApp extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [],
+      input: null
+    }
+    this.addItem=this.addItem.bind(this);
+    this.deleteItem=this.deleteItem.bind(this);
+    this.state.tasks.push({key: 1, value:"learn react"});
+    this.state.tasks.push({key: 2, value:"Go shopping"});
+    this.state.tasks.push({key: 3, value:"add to do tasks"});
+  }
+
+  addItem(toDoItem) {
+    var newList = this.state.tasks;
+    newList.push({key: this.state.tasks.length+1, value: toDoItem});
+    this.setState({tasks: newList});
+    console.log(this.state.tasks)
+  }
+
+  deleteItem(item) {
+    console.log(item)
+  }
+
+  render(){
+    return(
+      <div className = "container">
+        <h1>Todo List</h1>
+        <AddTodo input={this.state.input} addItem={this.addItem}/>
+        <TaskList tasks = {this.state.tasks} delete={this.deleteItem} />
+      </div>
+    );
+  }
+}
+
+
+
+
+export default ToDoApp;
